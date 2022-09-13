e2b0ff7e662128aa8dd993e67111206c63bf9f37
Ahmed Hawam <ahmed_hawam1998@outlook.com>
2022-09-13 17:08:29 +0200

    index

--- js/main.js
+++ js/main.js
@@ -1,28 +1,15 @@
-//=========== Active sidebar dashboard =============
-const currentLocationDashboard = location.href;
-const menuItemDashboard = document.querySelectorAll(
-  ".sidebar .nav-item .nav-link"
+//=========== Active navbar =============
+const currentLocation = location.href;
+const menuItem = document.querySelectorAll(
+  ".navbar .nav-item .nav-link"
 );
-const menuChildDashboard = document.querySelectorAll(
-  ".sidebar .nav-pills .nav-item ul li .nav-link"
-);
-const menuLengthDashboard = menuItemDashboard.length;
-for (let i = 0; i < menuLengthDashboard; i++) {
-  if (menuItemDashboard[i].href === currentLocationDashboard) {
-    menuItemDashboard[i].className = "nav-link active";
-    // menuChildDashboard[i].parentElement.parentElement.className =
-    //   "flex-column collapse show";
+const menuLength = menuItem.length;
+for (let i = 0; i < menuLength; i++) {
+  if (menuItem[i].href === currentLocation) {
+    menuItem[i].className = "nav-link active";
   }
 }
-//=========== Active sidebar dashboard =============
-
-//=========== sidebar toggle =============
-const sideBar = document.querySelector(".sidebar");
-const toggler = document.querySelector(".menu");
-toggler.addEventListener("click", function () {
-  sideBar.classList.toggle("open");
-});
-//=========== sidebar toggle =============
+//=========== Active navbar =============
 
 //=========== Close inspect =============
 // document.addEventListener("contextmenu", (event) => event.preventDefault());
@@ -41,20 +28,3 @@ toggler.addEventListener("click", function () {
 //   }
 // };
 //=========== Close inspect =============
-
-$(document).ready(function () {
-  $(".sidebar").hover(
-    function () {
-      $(".sidebar").addClass("opened");
-    },
-    function () {
-      $(".sidebar").removeClass("opened");
-    }
-  );
-});
-
-$(document).ready(function () {
-  $(".close").on("click", function () {
-    $(".sidebar.open .nav-pills .nav-item ul").removeClass("show");
-  });
-});
